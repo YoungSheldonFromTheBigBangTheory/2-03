c6b55b6756e6f776b0b6d7187193d87cce9d61c3
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-03-07 16:05:58 +0000

    h

--- giggle.py
+++ giggle.py
@@ -1,1 +1,17 @@
 
+
+name = input("What is your name?")
+age = int(input("How old are you?"))
+bday = str(input("Did you have your birthday? yes or no?"))
+
+    
+
+
+
+
+if bday == "yes" and "Yes":
+    year = 2023 - age
+    print (year)
+else:
+    h = 2022 - age
+    print(h)
